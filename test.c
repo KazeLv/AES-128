dff1bd8b67ea9b31f1e7689b83b8963dcf2b26ae
Wind Lv <kaze_lvzf@outlook.com>
2019-06-10 12:43:28 +0800

    recover

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
+
 	unsigned char c = 0x16;
+
 	printf("left: %hx\n", (unsigned char)(c / 16));
+
 	printf("right: %hx", (unsigned char)(c % 16));
 }
